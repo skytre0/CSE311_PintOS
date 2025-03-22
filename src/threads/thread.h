5bd2eed39ed2f13a7524fbb8cb4d451ec1ca1ecf
yangsj <yangsj0714@gmail.com>
2025-03-22 21:57:39 +0900

    init

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -93,8 +93,6 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
-    int64_t waketime;
-
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
