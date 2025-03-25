e2ea50ee8f7424482a01a05480e2d3d84e6fdba6
foxisdog <foxisdogmung@gmail.com>
2025-03-25 19:03:24 +0900

    suggested order argument passing and process wait

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -88,6 +88,7 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
+  while(true){} // 임시로 막아놓음
   return -1;
 }
 
@@ -437,7 +438,8 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+        // *esp = PHYS_BASE; 원본
+        *esp = PHYS_BASE - 12; //임시
       else
         palloc_free_page (kpage);
     }
